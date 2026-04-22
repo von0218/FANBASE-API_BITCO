177ecf247f5fe1f30786a5a7db886299fb470e8e
von0218 <bitco.yvonne123@gmail.com>
2026-04-23 00:47:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Adventure Time Fanbase API")
 
-# MOUNTING THE ROOT: This tells FastAPI to look in your main folder for the PNGs
-# We use "." to represent the current directory
+# Mount root for images
 app.mount("/static", StaticFiles(directory="."), name="static")
-
 templates = Jinja2Templates(directory="templates")
 
 @app.on_event("startup")
@@ -38,17 +36,28 @@ def seed_data():
             db.add_all(actors.values())
             db.commit()
 
+            # UPDATED ABILITIES BASED ON WIKI
             characters = [
-                models.Character(name="Finn the Human", species="Human", actor_id=actors["jeremy"].id),
-                models.Character(name="Jake the Dog", species="Magical Dog", actor_id=actors["john"].id),
-                models.Character(name="Princess Bubblegum", species="Gum Person", actor_id=actors["hynden"].id),
-                models.Character(name="Marceline the Vampire Queen", species="Vampire/Demon", actor_id=actors["olivia"].id),
-                models.Character(name="Ice King", species="Wizard", actor_id=actors["tom"].id),
-                models.Character(name="BMO", species="Console", actor_id=actors["niki"].id),
-                models.Character(name="Lumpy Space Princess", species="Lumpy Space Person", actor_id=actors["pendleton"].id),
-                models.Character(name="Flame Princess", species="Fire Elemental", actor_id=actors["jessica"].id),
-                models.Character(name="Lady Rainicorn", species="Rainicorn", actor_id=actors["niki"].id),
-                models.Character(name="Gunther", species="Penguin", actor_id=actors["tom"].id)
+                models.Character(name="Finn the Human", species="Human", actor_id=actors["jeremy"].id, 
+                                 ability="Master Swordsman & Heroism: Skilled in multiple martial arts and immune to the Lich's mind control."),
+                models.Character(name="Jake the Dog", species="Magical Dog", actor_id=actors["john"].id, 
+                                 ability="Stretchy Powers: Can modify the size, shape, and dimensions of every part of his body."),
+                models.Character(name="Princess Bubblegum", species="Gum Person", actor_id=actors["hynden"].id, 
+                                 ability="Scientific Genius: Master of candy-based technology, alchemy, and biological engineering."),
+                models.Character(name="Marceline the Vampire Queen", species="Vampire/Demon", actor_id=actors["olivia"].id, 
+                                 ability="Soul Sucking & Shapeshifting: Can absorb souls, turn invisible, and transform into monstrous forms."),
+                models.Character(name="Ice King", species="Wizard", actor_id=actors["tom"].id, 
+                                 ability="Frigid Magic: Can create and manipulate ice/snow, fly with his beard, and use 'Wizard Vision'."),
+                models.Character(name="BMO", species="Console", actor_id=actors["niki"].id, 
+                                 ability="Digital Reality: Can transport people into video games and functions as a multi-purpose electronic device."),
+                models.Character(name="Lumpy Space Princess", species="Lumpy Space Person", actor_id=actors["pendleton"].id, 
+                                 ability="Lumpy Bite: Her bite can turn others into Lumpy Space people; immune to the effects of the Anti-Element."),
+                models.Character(name="Flame Princess", species="Fire Elemental", actor_id=actors["jessica"].id, 
+                                 ability="Pyrokinesis: Can generate massive amounts of fire and heat, and transform into a colossal fire giant."),
+                models.Character(name="Lady Rainicorn", species="Rainicorn", actor_id=actors["niki"].id, 
+                                 ability="Chromatic Flight: Can fly, pass through walls, and change the color of objects with her horn."),
+                models.Character(name="Gunther", species="Penguin", actor_id=actors["tom"].id, 
+                                 ability="Cosmic Horror: Actually Orgalorg, an ancient deity capable of planet-level destruction and soul manipulation.")
             ]
             db.add_all(characters)
             db.commit()
@@ -65,10 +74,6 @@ def read_root():
 async def get_ui(request: Request, db: Session = Depends(get_db)):
     try:
         characters = db.query(models.Character).all()
-        return templates.TemplateResponse(
-            request=request, 
-            name="index.html", 
-            context={"characters": characters}
-        )
+        return templates.TemplateResponse(request=request, name="index.html", context={"characters": characters})
     except Exception:
         return HTMLResponse(content=f"<pre>{traceback.format_exc()}</pre>", status_code=500)
