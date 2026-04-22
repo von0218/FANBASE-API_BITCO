1143241b8e8dd80eaa21e4853c6a323ad7498062
von0218 <bitco.yvonne123@gmail.com>
2026-04-23 01:38:22 +0800

    Enhance seed_data with more characters and actors
    Added additional characters and actors to seed data.

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ from sqlalchemy.orm import Session
 import models
 from database import engine, get_db, SessionLocal
 
-# Rebuilds tables automatically
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-# Mounts the root directory for your MP3 and PNGs
 app.mount("/static", StaticFiles(directory="."), name="static")
 templates = Jinja2Templates(directory="templates")
 
@@ -20,26 +17,33 @@ templates = Jinja2Templates(directory="templates")
 def seed_data():
     db = SessionLocal()
     try:
-        # If the database is empty, fill it with cards!
+        # Check if the database is missing characters
         if not db.query(models.Character).first():
             actors = {
                 "jeremy": models.Actor(name="Jeremy Shada"),
                 "john": models.Actor(name="John DiMaggio"),
                 "hynden": models.Actor(name="Hynden Walch"),
-                "olivia": models.Actor(name="Olivia Olson")
+                "olivia": models.Actor(name="Olivia Olson"),
+                "tom": models.Actor(name="Tom Kenny"),
+                "niki": models.Actor(name="Niki Yang"),
+                "pendleton": models.Actor(name="Pendleton Ward"),
+                "jessica": models.Actor(name="Jessica DiCicco")
             }
             db.add_all(actors.values())
             db.commit()
 
+            # The Full Character List based on GitHub files
             characters = [
-                models.Character(name="Finn the Human", species="Human", actor_id=actors["jeremy"].id, 
-                                 ability="Master Swordsman & Heroism: Skilled in multiple martial arts and immune to the Lich's mind control."),
-                models.Character(name="Jake the Dog", species="Magical Dog", actor_id=actors["john"].id, 
-                                 ability="Stretchy Powers: Can modify the size, shape, and dimensions of every part of his body."),
-                models.Character(name="Princess Bubblegum", species="Gum Person", actor_id=actors["hynden"].id, 
-                                 ability="Scientific Genius: Master of candy-based technology, alchemy, and biological engineering."),
-                models.Character(name="Marceline the Vampire Queen", species="Vampire/Demon", actor_id=actors["olivia"].id, 
-                                 ability="Soul Sucking & Shapeshifting: Can absorb souls, turn invisible, and transform into monstrous forms.")
+                models.Character(name="Finn the Human", species="Human", actor_id=actors["jeremy"].id, ability="Heroism & Master Swordsmanship"),
+                models.Character(name="Jake the Dog", species="Magical Dog", actor_id=actors["john"].id, ability="Elastic Stretchy Powers"),
+                models.Character(name="Princess Bubblegum", species="Gum Person", actor_id=actors["hynden"].id, ability="Scientific Genius & Alchemy"),
+                models.Character(name="Marceline", species="Vampire", actor_id=actors["olivia"].id, ability="Soul Sucking & Shapeshifting"),
+                models.Character(name="Ice King", species="Wizard", actor_id=actors["tom"].id, ability="Snow & Ice Manipulation"),
+                models.Character(name="BMO", species="Console", actor_id=actors["niki"].id, ability="Digital Reality & Video Games"),
+                models.Character(name="Lumpy Space Princess", species="Lumpy Space Person", actor_id=actors["pendleton"].id, ability="Lumpy Bite & Floating"),
+                models.Character(name="Flame Princess", species="Fire Elemental", actor_id=actors["jessica"].id, ability="Pyrokinesis & Heat Generation"),
+                models.Character(name="Lady Rainicorn", species="Rainicorn", actor_id=actors["niki"].id, ability="Chromatic Flight & Phasing"),
+                models.Character(name="Gunther", species="Penguin", actor_id=actors["tom"].id, ability="Cosmic Slap & Hidden Eldritch Power")
             ]
             db.add_all(characters)
             db.commit()
